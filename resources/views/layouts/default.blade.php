54fca1e69c5f121740ee23108aa8c6e55e68dbea
borjantj <89358667+borjantj@users.noreply.github.com>
2021-09-23 13:44:42 +0200

    Update #1
    Footer is gecentered en staat onderaan de page. Text is wit op achtergrond.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -17,7 +17,7 @@
 
             <ul class="breadcrumb">
                 <li><a href="/" title="{{ __('misc.home_alt') }}"
-                       alt="{{ __('misc.home_alt') }}">{{ __('misc.home') }}</a></li>
+                        alt="{{ __('misc.home_alt') }}">{{ __('misc.home') }}</a></li>
                 @yield('breadcrumb')
             </ul>
 
